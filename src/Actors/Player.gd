578beba03cae233e317d994bed606fe21e81b5c0
Alberto Oliveira <alberto.a.oliv@gmail.com>
2022-09-23 21:12:35 -0300

    Test commit on godot

---
+++ src/Actors/Player.gd
@@ -0,0 +1,38 @@
+extends Actor
+
+
+func _physics_process(delta: float) -> void:
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var direction: = get_direction()
+	var test: = 5
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+
+func jump_action():
+	
+	return Input.get_action_strength("jump") and is_on_floor()
+
+func get_direction() -> Vector2:
+	
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if jump_action() else 1.0
+	)
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+	
+	var out: = linear_velocity
+	out.x = speed.x * direction.x
+	out.y += gravity * get_physics_process_delta_time()
+	#out.y = max(out.y, speed.y)
+	if direction.y == -1.0:
+		out.y = speed.y * direction.y
+	if is_jump_interrupted:
+		out.y = 0.0
+		
+	return out
